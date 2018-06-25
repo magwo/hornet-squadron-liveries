44efbddd68312749f4375e11e5aa05978db543df
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2018-06-25 20:47:28 +0200

    Initial commit

---
+++ generate.js
@@ -0,0 +1,17 @@
+
+console.log("Create output dir");
+
+console.log("Create temp dir");
+
+console.log("Read squadron definition");
+
+console.log("Read description.lua");
+
+console.log("Convert base skin files to png or similar, put in temp folder");
+
+console.log("For each member:");
+console.log("  Create member dir in temp dir");
+console.log("  Copy base pngs to member temp dir");
+console.log("  Add text decals tp temp pngs");
+console.log("  Export member pngs to DDS files (DXT5?) to member output dir");
+console.log("  Write modified description.lua to member output dir");
