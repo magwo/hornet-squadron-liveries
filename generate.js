681b23af21070b90b9ebbf7cfb8f25c3b575623c
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2018-06-26 00:04:43 +0200

    Somewhat working skin generation

--- generate.js
+++ generate.js
@@ -1,5 +1,5 @@
 const fs = require("fs-extra");
-const gm = require("gm");
+const gm = require("gm").subClass({imageMagick: true});
 
 var squad;
 var description;
@@ -18,12 +18,20 @@ function init() {
     console.log("Reading description lua file");
     description = fs.readFileSync(squad.livery_source_description_lua, "utf8");
     console.log("Description is", description);
+
 }
 
-console.log("Convert base skin files to png or similar, put in temp folder");
+function createPng(fileBaseName, done) {
+    console.log(`Converting ${fileBaseName} to png, putting in temp folder`);
+    gm(`${__dirname}/${squad.livery_source_dir}/${fileBaseName}${squad.livery_source_file_suffix}.psd`)
+        .flatten()
+        .write(`${__dirname}/temp/${fileBaseName}${squad.livery_source_file_suffix}.png`, done);
+}
 
-function createBaseSkin() {
-
+function errorHandler(err) {
+    if(err) {
+        console.error(err);
+    }
 }
 
 
@@ -31,13 +39,25 @@ function createMemberSkin(m) {
     console.log("Processing member", m.callsign);
     const memberTempDir = `temp/${m.callsign}`;
     fs.ensureDirSync(memberTempDir);
-
-    console.log("  Copy base pngs to member temp dir");
-
-    console.log("  Add text decals tp temp pngs");
-
     const memberOutputDir = `output/${squad.livery_basedir} ${m.callsign}`
     fs.ensureDirSync(memberOutputDir);
+
+    const pilotNameText = `${m.rank} ${m.first} ${m.last}\n"${m.callsign}"`.toUpperCase();
+
+    console.log("  Adding text decals");
+    gm(`${__dirname}/temp/F18C_1_DIFF${squad.livery_source_file_suffix}.png`)
+        .draw([`font 'Arial Black.ttf' font-size 40 fill '#333333' gravity Center text -600,-800 '${pilotNameText}'`])
+        .draw([`font 'Arial Black.ttf' font-size 40 fill '#333333' gravity Center rotate 180 text 600,400 '${pilotNameText}'`])
+        .write(`${__dirname}/${memberOutputDir}/F18C_1_DIFF${squad.livery_source_file_suffix}.dds`, errorHandler);
+
+    gm(`${__dirname}/temp/F18C_2_DIFF${squad.livery_source_file_suffix}.png`)
+        .font("Arial Black.ttf", 60)
+        .stroke("#333333")
+        .fill("#333333")
+        .drawText(0, 0, m.callsign.toUpperCase(), "center")
+        .write(`${__dirname}/${memberOutputDir}/F18C_2_DIFF${squad.livery_source_file_suffix}.dds`, errorHandler);
+
+    
 
     console.log("  Constructing and writing member description lua", m.callsign);
     const memberDesc = description.replace(/name = \"[^\"]*\"/, `name = "${squad.livery_basename} ${m.callsign}"`);
@@ -50,5 +70,19 @@ function createMemberSkin(m) {
 
 
 init();
-createBaseSkin();
-squad.members.forEach(createMemberSkin);
+createPng("F18C_1_DIFF", function(err) {
+    if(err) {
+        console.log(err);
+        process.exit(1);
+    }
+    createPng("F18C_2_DIFF", function(err) {
+        if(err) {
+            console.log(err);
+            process.exit(1);
+        }
+        console.log("Done writing pngs");
+        if(!err) {
+            squad.members.forEach(createMemberSkin);
+        }
+    });
+});
