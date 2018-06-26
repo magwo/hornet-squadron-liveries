89164585f648649126425785b7a9d44d6d3523d7
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2018-06-26 22:57:54 +0200

    Much better generation.. I think?

--- generate.js
+++ generate.js
@@ -22,16 +22,23 @@ function init() {
 }
 
 function createPng(fileBaseName, done) {
-    console.log(`Converting ${fileBaseName} to png, putting in temp folder`);
-    gm(`${__dirname}/${squad.livery_source_dir}/${fileBaseName}${squad.livery_source_file_suffix}.psd`)
-        .flatten()
-        .write(`${__dirname}/temp/${fileBaseName}${squad.livery_source_file_suffix}.png`, done);
+    return done();
+    // console.log(`Converting ${fileBaseName} to png, putting in temp folder`);
+    // gm(`${__dirname}/${squad.livery_source_dir}/${fileBaseName}${squad.livery_source_file_suffix}.psd`)
+    //     .flatten()
+    //     .write(`${__dirname}/temp/${fileBaseName}${squad.livery_source_file_suffix}.png`, done);
 }
 
 function errorHandler(err) {
     if(err) {
         console.error(err);
     }
+}
+
+function copyFiles(outputDir) {
+    squad.files_to_copy.forEach(function(f) {
+        fs.copyFileSync(`${squad.livery_source_dir}/${f}`, `${outputDir}/${f}`);
+    })
 }
 
 
@@ -44,16 +51,16 @@ function createMemberSkin(m) {
 
     const pilotNameText = `${m.rank} ${m.first} ${m.last}\n"${m.callsign}"`.toUpperCase();
 
-    console.log("  Adding text decals");
+    console.log("  Adding text decals and exporting to DDS files");
     gm(`${__dirname}/temp/F18C_1_DIFF${squad.livery_source_file_suffix}.png`)
-        .draw([`font 'Arial Black.ttf' font-size 40 fill '#333333' gravity Center text -600,-800 '${pilotNameText}'`])
-        .draw([`font 'Arial Black.ttf' font-size 40 fill '#333333' gravity Center rotate 180 text 600,400 '${pilotNameText}'`])
+        .draw([`font 'Arial Black.ttf' font-size 25 fill '#444' translate ${730-2048},${1890-2048} gravity Center rotate 184 text 0,0 '${pilotNameText}'`])
+        .draw([`font 'Arial Black.ttf' font-size 25 fill '#444' translate ${730-2048},${1060-2048} gravity Center rotate 356 text 0,0 '${pilotNameText}'`])
         .write(`${__dirname}/${memberOutputDir}/F18C_1_DIFF${squad.livery_source_file_suffix}.dds`, errorHandler);
 
     gm(`${__dirname}/temp/F18C_2_DIFF${squad.livery_source_file_suffix}.png`)
         .font("Arial Black.ttf", 60)
-        .stroke("#333333")
-        .fill("#333333")
+        .stroke("#444")
+        .fill("#444")
         .drawText(0, 0, m.callsign.toUpperCase(), "center")
         .write(`${__dirname}/${memberOutputDir}/F18C_2_DIFF${squad.livery_source_file_suffix}.dds`, errorHandler);
 
@@ -62,10 +69,8 @@ function createMemberSkin(m) {
     console.log("  Constructing and writing member description lua", m.callsign);
     const memberDesc = description.replace(/name = \"[^\"]*\"/, `name = "${squad.livery_basename} ${m.callsign}"`);
     fs.writeFileSync(`${memberOutputDir}/${squad.livery_source_description_lua}`, memberDesc, "utf8");
-    
 
-    console.log("  Export member pngs to DDS files (DXT5?) to member output dir");
-    console.log("  Write modified description.lua to member output dir");
+    copyFiles(memberOutputDir);
 }
 
 
